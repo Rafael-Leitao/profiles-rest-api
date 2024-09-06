071d61b9ab43645ff2e7488ab8425522bf99c5e5
Rafael <rafael.s.leitao@gmail.com>
2024-09-06 13:34:37 -0700

    Changes made to the setup.sh file 2

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,22 +10,31 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
+# Install Python, SQLite, and pip
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
+# Create the project directory and clone the repository
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
+# Create a Python virtual environment
 python3 -m venv $PROJECT_BASE_PATH/env
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Activate the virtual environment
+source $PROJECT_BASE_PATH/env/bin/activate
+
+# Install the required packages from requirements.txt, with a specific uwsgi version
+# pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+
+# If you encounter issues with uwsgi, consider installing an alternative like gunicorn:
+pip install -r $PROJECT_BASE_PATH/requirements.txt gunicorn
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
-# Setup Supervisor to run our uwsgi process.
+# Setup Supervisor to run our uwsgi (or gunicorn) process.
 cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
